ba571b07f841d381e30060fa9626c3f360258ea6
KhaledAli1234 <kemam982@gmail.com>
2025-10-18 00:38:52 +0300

    commit

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
     @Test
     void testAdd() {
+
         Calculator calc = new Calculator();
         assertEquals(5, calc.add(2, 3));
     }
